5fc1c6969207f96ac3ab81b99382a562ef042fab
Margarita Herrera <mariaherrera2019@gmail.com>
2020-09-17 18:57:12 +0000

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const { Router } = require ('express');
+const path = require('path');
+const router = Router();
+
+
+router.get('/Saludo', (req,res) =>{
+  res.send('Mi primera clase Backend');
+})
+
+router.get('/Saludo', (req,res) =>{
+  res.send('Hola Jenny Bienvenida');
+})
+
+module.exports = router;
